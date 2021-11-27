e428726f34b13d84b8648e96e1f10ea8e2ee76d5
gauravappy97 <gauravappy1997@gmail.com>
2021-11-27 18:35:29 +0530

    profile updated

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
 	});
 
 	var typed = new Typed(".typed",{
-		strings: ["System Software Engineer @TCS Digital","Computer Science Undergraduate Student.","Web Developer.","Beginner In The Field Of Data Science.","Python Programming Enthusiast."],
+		strings: ["System Software Engineer @TCS Digital","Computer Science Graduate.","Full Stack Developer.","Beginner In The Field Of Data Science.","Python Programming Enthusiast."],
 		typeSpeed: 50,
 		loop: true,
 		startDelay: 1000,
